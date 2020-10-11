872971f97f0a5b765b3043a7f6fd40253c8aa5ff
Davi Almeida <davi221.almeida@hotmail.com>
2020-10-11 18:56:16 -0300

    Added database connection

---
+++ src/main/java/ConnectDB.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+    private static final String DRIVER = "org.postgresql.Driver";
+    private static final String HOST = System.getenv("DATABASE_HOST");
+    private static final String PORT = System.getenv("DATABASE_PORT");
+    private static final String DATABASENAME = System.getenv("DATABASE_NAME");
+    private static final String USERNAME = System.getenv("DATABASE_USER");
+    private static final String PASSWORD = System.getenv("DATABASE_PASSWORD");
+    private static final String URL = "jdbc:postgresql://"+HOST+":"+PORT+"/"+DATABASENAME+"?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Where is your PostgreSQL JDBC Driver? Include in your pom.xml");
+            return null;
+        }
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
--- src/main/java/helloServlet.java
+++ src/main/java/helloServlet.java
@@ -4,11 +4,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
-@WebServlet(urlPatterns = "/helloServlet")
+@WebServlet(name = "HelloServlet", urlPatterns = "/helloServlet")
 public class helloServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("Sup, mate!");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection connection = null;
+        try {
+            connection = ConnectDB.getConnection();
+        } catch (SQLException ex) {
+            resp.getWriter().append("Connection Failed! Check output console");
+        }
+        if(connection!= null){
+            resp.getWriter().append("A conexão com o banco foi realizada!");
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }else{
+            resp.getWriter().append("A conexão com o banco falhou!");
+        }
     }
 }
