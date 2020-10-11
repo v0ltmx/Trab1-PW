138a4be27dd9f2fcff0d89687e47190cb8bb2242
Davi Almeida <davi221.almeida@hotmail.com>
2020-10-11 19:21:46 -0300

    Added database connection

--- src/main/java/ConnectDB.java
+++ src/main/java/ConnectDB.java
@@ -6,10 +6,10 @@ public class ConnectDB {
     private static final String DRIVER = "org.postgresql.Driver";
     private static final String HOST = System.getenv("DATABASE_HOST");
     private static final String PORT = System.getenv("DATABASE_PORT");
-    private static final String NAME = System.getenv("DATABASE_NAME");
+    private static final String DATABASENAME = System.getenv("DATABASE_DATABASE");
     private static final String USERNAME = System.getenv("DATABASE_USER");
     private static final String PASSWORD = System.getenv("DATABASE_PASSWORD");
-    private static final String URL = "jdbc:postgresql://"+HOST+":"+PORT+"/"+NAME+"?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+    private static final String URL = "jdbc:postgresql://"+HOST+":"+PORT+"/"+DATABASENAME+"?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName(DRIVER);
